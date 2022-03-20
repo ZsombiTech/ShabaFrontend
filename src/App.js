021cf701ffed35afa51df8de2db9cc5213457e2d
ZsombiTech <horvathzsombor2005@gmail.com>
2022-03-20 20:23:26 +0100

    ....

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import HeadNavbar from "./components/HeadNavbar";
+import Login from "./components/Login";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <HeadNavbar />
+      <div className="centered">
+        <Login />
+      </div>
+    </div>
+  );
 }
 
 export default App;
