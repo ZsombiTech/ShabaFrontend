ad7207fd2d5e0dfc6ea973ceaafe7302e72bdd32
ZsombiTech <horvathzsombor2005@gmail.com>
2022-04-02 06:12:42 +0200

    done for today

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,5 +1,9 @@
 import React from "react";
 
-export default function Loading() {
-  return <h1 className="mt-36">Loading</h1>;
+export default function Loading(props) {
+  let text = "Loading";
+  if (props.truee) {
+    text = "You haven't posted anything yet...";
+  }
+  return <h1 className="mt-36">{text}</h1>;
 }
